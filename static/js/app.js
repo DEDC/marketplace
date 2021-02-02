590c42f516491d5e8e77c9137e7427d8025f8222
daviddc <dedc_011596@hotmail.com>
2021-02-02 13:11:51 -0600

    Cambio de acomodo de información del collapsible

--- static/js/app.js
+++ static/js/app.js
@@ -2,12 +2,10 @@ var sidenav = document.querySelectorAll('.sidenav');
 var sv_instances = M.Sidenav.init(sidenav);
 var collapsible = document.querySelectorAll('.collapsible');
 var col_instances = M.Collapsible.init(collapsible);
-var dropdown = document.querySelectorAll('.dropdown-trigger');
-var drp_instances = M.Dropdown.init(dropdown);
 var select = document.querySelectorAll('select');
 var select_instances = M.FormSelect.init(select);
-var collapsible = document.querySelectorAll('.collapsible');
-var instances_collapsible = M.Collapsible.init(collapsible);
+var dropdown = document.querySelectorAll('.dropdown-trigger');
+var drp_instances = M.Dropdown.init(dropdown, { 'coverTrigger': false });
 var not_collapse = document.querySelectorAll('.not-collapse')
 not_collapse.forEach(e => {
     e.addEventListener('click', function (ev) {
