d0cd21e008d7d6e9eba43ad1ddab013864e33f80
daviddc <dedc_011596@hotmail.com>
2021-04-06 14:01:26 -0500

    Deshabilitación del botón de compra al realizar una

--- static/js/stripe/index.js
+++ static/js/stripe/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
-var stripe = Stripe('pk_live_51I7T2kE4OKuuVvsDF4talTmj4SYyhYfulAJvPsknmlHvDKqcBMxdrVlVsErxqYUx09qRCOQ0CWwUmB2NLa9ZSXGR00AQRGb5Tt')
+//var stripe = Stripe('pk_live_51I7T2kE4OKuuVvsDF4talTmj4SYyhYfulAJvPsknmlHvDKqcBMxdrVlVsErxqYUx09qRCOQ0CWwUmB2NLa9ZSXGR00AQRGb5Tt')
+var stripe = Stripe('pk_test_51I7T2kE4OKuuVvsDzt6ZZA4jnOq18zmAKFsNh7p3zJ8qAOoPPppvnPOGXQcfGBHCLoxupyKCsWg6flBeKFXreuHD00vOFme0XO')
 var elements = stripe.elements()
 var style = {
   base: {
@@ -21,26 +22,51 @@ var style = {
 var card = elements.create('card', { style: style })
 var card_name = document.querySelector('#cardname')
 var cont_payment = document.querySelector('#cont-payment')
-var form = document.getElementById('payment-form')
+const form = document.getElementById('payment-form')
 var selected_card = document.querySelector('#card')
 var add_method = document.querySelector('#add_method')
 var remove_method = document.querySelector('#remove_method')
+var btn = form.querySelector('#btn-pay')
+var progress = document.querySelector('.progress')
+progress.style.display = 'none'
 
 if (!selected_card.value) {
   card.mount('#card-element')
-  formPaymentSubmit()
 }
 else {
   cont_payment.style.display = 'none'
   card_name.required = false
 }
 
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+  let card_element = document.querySelector('#card-element')
+  if (card_element) {
+    if (card_element.classList.contains('StripeElement')) {
+      stripe.createToken(card, { name: card_name.value }).then(function (result) {
+        if (result.error) {
+          var errorElement = document.getElementById('card-errors')
+          errorElement.textContent = result.error.message
+        } else {
+          btn.disabled = true
+          progress.style.display = 'block'
+          stripeTokenHandler(result.token)
+        }
+      })
+    }
+    else if (selected_card.value) {
+      btn.disabled = true
+      progress.style.display = 'block'
+      form.submit()
+    }
+  }
+})
+
 add_method.addEventListener('click', function (e) {
   e.preventDefault()
   card.mount('#card-element')
   cont_payment.style.display = 'block'
   card_name.required = true
-  formPaymentSubmit()
 })
 
 remove_method.addEventListener('click', function (e) {
@@ -48,22 +74,7 @@ remove_method.addEventListener('click', function (e) {
   card.unmount()
   cont_payment.style.display = 'none'
   card_name.required = false
-  form.removeEventListener('submit', function () { })
 })
-
-function formPaymentSubmit() {
-  form.addEventListener('submit', function (event) {
-    event.preventDefault()
-    stripe.createToken(card, { name: card_name.value }).then(function (result) {
-      if (result.error) {
-        var errorElement = document.getElementById('card-errors')
-        errorElement.textContent = result.error.message
-      } else {
-        stripeTokenHandler(result.token)
-      }
-    })
-  })
-}
 
 function stripeTokenHandler(token) {
   var hiddenInput = document.createElement('input')
